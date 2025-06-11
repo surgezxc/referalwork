49fc74e741e2facad8c834bfea7036e4f86ee473
surgezxc <163468487+surgezxc@users.noreply.github.com>
2025-06-11 19:00:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Можно добавить простой функционал, например, плавную прокрутку
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
